8856d65b3094c2cdb042c8a0b65d8bc057dd2781
nsalminen <n.salminen@me.com>
2020-07-15 10:40:49 +0200

    Add initial version of Python Flask app

---
+++ app.py
@@ -0,0 +1,161 @@
+import sys
+import time
+import logging
+import random
+from flask import Flask, render_template, jsonify, request, redirect, url_for, session
+import time
+import markovify
+import os
+import json
+import signal
+from tqdm import tqdm
+from datetime import datetime, date
+
+app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+dataDir = "data"
+
+emotions = ["joy", "sadness", "anger", "surprise"]
+models = {}
+
+
+@app.route('/')
+def root():
+    session['emotion'] = ''
+    args = {}
+    d0 = date(2020, 6, 18)
+    now = date.today()
+    delta = now - d0
+    args["days"] = (delta.days)
+
+    args["total"] = 0
+    for emotion in emotions:
+        with open(os.path.join(dataDir, f"{emotion}.json"), 'rt') as f:
+            try:
+                entries = json.load(f)
+                args[emotion] = len(entries.keys())
+                args["total"] += args[emotion]
+            except ValueError:
+                args[emotion] = 0
+                print(f"Could not retrieve data file for {emotion}.")
+
+    return render_template('main.html', **args)
+
+
+def renderEmotion(emotion):
+    session['emotion'] = emotion
+    with open(os.path.join(dataDir, f"{emotion}.json"), 'rt') as f:
+        try:
+            entries = json.load(f)
+            count = len(entries.keys())
+            # print(entries)
+        except ValueError:
+            count = 0
+            print(f"Could not retrieve data file for {emotion}.")
+
+    corpus = [(v['message'], f"{v['name']}, {v['title']}", str(k))
+              for k, v in entries.items()]
+    return render_template(f'{emotion}.html', count=count, corpus=corpus)
+
+
+@app.route('/joy')
+def renderJoy():
+    return renderEmotion('joy')
+
+
+@app.route('/sadness')
+def renderSadness():
+    return renderEmotion('sadness')
+
+
+@app.route('/anger')
+def renderAnger():
+    return renderEmotion('anger')
+
+
+@app.route('/surprise')
+def renderSurprise():
+    return renderEmotion('surprise')
+
+
+@app.route('/output')
+def output():
+    return render_template('output.html', sentence="Welcome.")
+
+
+@app.route("/getSentence.json")
+def getSentence():
+    print(session['emotion'])
+    if session['emotion'] in models:
+        sentence = models[session['emotion']].make_sentence(tries=20)
+        if sentence is not None:
+            return (str(sentence))
+        else:
+            return "Please enter an additional sentence."
+    else:
+        return "Please select an emotion."
+
+
+@app.route('/stopServer', methods=['GET'])
+def stopServer():
+    os.kill(os.getpid(), signal.SIGINT)
+    return jsonify({"success": True, "message": "Server is shutting down..."})
+
+
+def rebuild_model(emotion, corpus):
+    print(f"Building model for {emotion}...")
+    models[emotion] = markovify.Text(corpus)
+
+
+@app.route('/entry', methods=['POST', 'DELETE'])
+def entry():
+    if request.method == 'POST':
+        now = str(datetime.now())
+        entry = {}
+        entry[now] = {}
+        entry[now]['message'] = request.form['message']
+        entry[now]['name'] = request.form['name']
+        entry[now]['title'] = request.form['title']
+        emotion = session['emotion']
+        print(emotion)
+        fname = f'../data/{emotion}.json'
+        with open(fname, 'rt') as f:
+            try:
+                entries = json.load(f)
+            except ValueError:
+                entries = {}
+            entries.update(entry)
+
+        with open(fname, "wt") as f:
+            json.dump(entries, f, indent=2)
+
+        newMessages = [v['message'] for k, v in entries.items()]
+        rebuild_model(emotion, newMessages)
+        return redirect(url_for(f'render{emotion.capitalize()}'))
+    elif request.method == 'DELETE':
+        timestamp = request.form['timestamp']
+        emotion = session['emotion']
+        fname = f'../data/{emotion}.json'
+        with open(fname, 'rt') as f:
+            try:
+                entries = json.load(f)
+            except ValueError:
+                entries = {}
+            entries.pop(timestamp)
+
+        with open(fname, "wt") as f:
+            json.dump(entries, f, indent=2)
+        return jsonify(isError=False,
+                       message=f"Deleted entry with timestamp {timestamp}",
+                       statusCode=200), 200
+
+
+for emotion in emotions:
+    with open(os.path.join(dataDir, f"{emotion}.json"), 'rt') as f:
+        try:
+            entries = json.load(f)
+            messages = [v['message'] for k, v in entries.items()]
+            rebuild_model(emotion, messages)
+        except ValueError:
+            print(f"Could not build model for {emotion}.")
---
+++ static/assets/js/emotions.js
@@ -0,0 +1,131 @@
+$(document).ready(function () {
+  //UI 상수
+  var write = document.getElementById("write");
+  var close = document.getElementById("close");
+  var save = document.getElementById("save");
+  var deletes = document.getElementById("delete");
+  var pop_Layer = document.getElementsByClassName("pop-up");
+
+  //pop-up 데이터 상수
+  var pageTitle = document.title;
+  var author = document.getElementsByName("author")[0];
+  var title = document.getElementsByName("title")[0];
+  var corpus = document.getElementsByName("corpus")[0];
+  var count = document.getElementsByClassName("counts")[0];
+
+  //팝업창 보이기
+  write.onclick = function () {
+    pop_Layer[0].style.display = "block";
+  };
+
+  //삭제창
+  var isClicked = false;
+  var delete_icon = document.getElementById("deleteImg");
+
+  deletes.addEventListener("click", function () {
+    if (isClicked) {
+      //삭제 모드 취소
+      isClicked = false;
+      //img change
+      delete_icon.src = "/static/assets/img/delete_icon.png";
+      //스크롤 다시 시작
+      doScroll();
+      autoScroll();
+      //이벤트 제거
+      clearDeleteMode();
+    } else {
+      //삭제 모드
+      isClicked = true;
+      //img change
+      delete_icon.src = "/static/assets/img/delete_save_icon.png";
+      //스크롤 멈춤
+      stopScroll();
+      //삭제 화면
+      deleteMode();
+    }
+  });
+
+  //textarea 내용 지우고 팝업창 닫기
+  function clear() {
+    author.value = "";
+    title.value = "";
+    corpus.value = "";
+    corpus.style.borderColor = "black";
+    pop_Layer[0].style.display = "none";
+  }
+
+  // var mySwiper = new Swiper(".swiper-container", {
+  //   direction: "vertical",
+  //   slidesPerView: "auto",
+  //   speed: 1200,
+  //   autoplay: {
+  //     delay: 1000,
+  //     disableOnInteraction: false,
+  //   }
+  // });
+
+  var contentsList = document.getElementsByClassName("contents");
+
+  function deleteContents() {
+    console.log(this.dataset.timestamp);
+    $.ajax({
+      url: "/entry",
+      method: "delete",
+      data: {
+        timestamp: this.dataset.timestamp,
+      },
+      success: function () {
+        location.reload();
+      },
+    });
+  }
+
+  function mouseoverEvent() {
+    this.style.opacity = 0.13;
+    this.style.cursor = "pointer";
+  }
+
+  //attach eventlistener
+  function deleteMode() {
+    for (var i = 0; i < contentsList.length; i++) {
+      //hover effect
+      contentsList[i].onmouseout = function () {
+        this.style.opacity = 1;
+        this.style.cursor = "default";
+      };
+      contentsList[i].addEventListener("mouseover", mouseoverEvent);
+      contentsList[i].addEventListener("click", deleteContents);
+    }
+  }
+
+  //remove eventlistener
+  function clearDeleteMode() {
+    for (var i = 0; i < contentsList.length; i++) {
+      contentsList[i].removeEventListener("mouseover", mouseoverEvent);
+      contentsList[i].removeEventListener("click", deleteContents);
+    }
+  }
+
+  var target = document.getElementsByClassName("contents_container")[0];
+  var isScroll = true;
+
+  var myTimer;
+  var autoScroll = function () {
+    target.scrollTop += 1;
+    myTimer = setTimeout(autoScroll, 50);
+  };
+
+  //스크롤 재시작
+  var doScroll = function () {
+    isScroll = true;
+    //스크롤 위로 올리기
+    target.scrollTop = 0;
+  };
+  //스크롤 멈춤
+  var stopScroll = function () {
+    isScroll = false;
+    clearTimeout(myTimer);
+  };
+
+  autoScroll();
+});
